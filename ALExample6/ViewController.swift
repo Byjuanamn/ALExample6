ea30dcc67b2b907863be0e157c067a16446b0125
byjuanamn <juan.antonio.martin@gmail.com>
2016-06-27 01:41:07 +0200

    primer commit

---
+++ ALExample6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALExample6
+//
+//  Created by Juan Antonio Martin Noguera on 27/06/16.
+//  Copyright © 2016 Com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
